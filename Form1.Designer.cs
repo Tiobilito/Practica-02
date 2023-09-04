a9d4d53631793edf70ba7cd37365331e411a41dc
Tiobilito <andre.marin1135@alumno.udg.mx>
2023-09-04 16:47:53 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace Práctica_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(258, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Seleccionar una capeta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(268, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Caos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 36);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(240, 211);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 308);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Práctica_02
+{
+    public partial class Form1 : Form
+    {
+        string selectedfolder;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string newName(string cad)
+        {
+            string newcad="";
+            int numRan,i;
+            Random rnd = new Random();
+            for (i=0; i<cad.Length; i++)
+            {
+                if(cad[i]<123 && cad[i]>64)
+                {
+                    numRan = rnd.Next(65, 123);
+                    newcad += Convert.ToChar(numRan);
+                } else
+                {
+                    numRan = rnd.Next(48, 57);
+                    newcad += Convert.ToChar(numRan);
+                }
+            }
+            return newcad;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] archives;
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK) 
+            { 
+                selectedfolder = fbd.SelectedPath;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int indice;
+            string[] archivos;
+            string Nombre;
+            archivos = Directory.GetFiles(selectedfolder);
+            for (int i = 0; i < archivos.Length; i++)
+            {
+                indice = archivos[i].LastIndexOf("\\") + 1;
+                Nombre = archivos[i].Substring(indice);
+                File.Copy(archivos[i], selectedfolder + Convert.ToChar(92) + newName(Path.GetFileNameWithoutExtension(Nombre)) + Path.GetExtension(archivos[i]));
+            }
+            archivos = Directory.GetDirectories(selectedfolder);
+            for (int i = 0; i < archivos.Length; i++)
+            {
+                indice = archivos[i].LastIndexOf("\\") + 1;
+                Nombre = archivos[i].Substring(indice);
+                Directory.CreateDirectory(selectedfolder + Convert.ToChar(92) + newName(Nombre));
+                richTextBox1.Text += Nombre + "\n";
+            }
+        }
+    }
+}
